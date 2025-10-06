556806b613fff468eaf4d18b1e47bf5784137502
moment-NEW <1838088566@qq.com>
2025-10-06 14:51:56 +0800

    保存，准备切出到底盘分支

--- Task/app_gimbal_task.c
+++ Task/app_gimbal_task.c
@@ -20,6 +20,7 @@ float target_up_position=0.0;
 uint8_t ControlMode= RC_MODE;
 float target_position=0.0,test_speed=0.0,test_position=0.0,target_speed=0.0;
 float target_up_position=0.0;
+float target_pitch_position=0.0;
 #endif
 ////////////////////////////电机配置/////////////////////////////////////////
 static DmMotorInitConfig_s Down_config = {
@@ -98,7 +99,7 @@ static DmMotorInitConfig_s pitch_config = {
         .kf = 0.0,
         .angle_max = 0,
         .i_max = 1000.0,
-        .out_max = 2000.0,
+        .out_max = 2000.0,//待商议
     }
 };
 static  DjiMotorInitConfig_s Up_config = {
@@ -115,7 +116,7 @@ static  DjiMotorInitConfig_s Up_config = {
     .reduction_ratio = 1,              // 减速比
 
     .angle_pid_config = {
-        .kp = 0.0,                        // 位置环比例系数
+        .kp = 160.0,                        // 位置环比例系数
         .ki = 0.0,                        // 位置环积分系数
         .kd = 0.0,                        // 位置环微分系数
         .kf = 0.0,                        // 前馈系数
@@ -124,13 +125,13 @@ static  DjiMotorInitConfig_s Up_config = {
         .out_max = 400.0,                 // 输出限幅(速度环输入)
     },
     .velocity_pid_config = {
-        .kp = 0.0,                       // 速度环比例系数
+        .kp = 100.0,                       // 速度环比例系数
         .ki = 0.0,                        // 速度环积分系数
         .kd = 0.0,                        // 速度环微分系数
         .kf = 0.0,                        // 前馈系数
         .angle_max = 0,                 // 角度最大值(限幅用，为0则不限幅)
         .i_max = 1000.0,                  // 积分限幅
-        .out_max = 2000.0,                // 输出限幅(电流输出)
+        .out_max = 16000,                // 输出限幅(电流输出)
     }
 };
 
@@ -156,18 +157,19 @@ void StartGimbalTask(void const * argument)
 //        Motor_Dm_Cmd(Down_yaw,  DM_CMD_MOTOR_ENABLE);
 //        Motor_Dm_Transmit(Down_yaw);
 //    }
-//	   while (pitch->motor_state!=DM_ENABLE)
-//    {
-//        Motor_Dm_Cmd(pitch,  DM_CMD_MOTOR_ENABLE);
-//        Motor_Dm_Transmit(pitch);
-//    }
+	   while (pitch->motor_state!=DM_ENABLE)
+    {
+        Motor_Dm_Cmd(pitch,  DM_CMD_MOTOR_ENABLE);
+        Motor_Dm_Transmit(pitch);
+				osDelay(1);
+    }
     Log_Information("pitch motor enable success\r\n");
  
   for(;;)
   {
 		#ifdef DEBUG
-		test_speed=Down_yaw->message.out_velocity;
-		test_position=Down_yaw->message.out_position;
+		test_speed=pitch->message.out_velocity;
+		test_position=pitch->message.out_position;
 		#endif
 		switch (ControlMode) {
 			case PC_MODE:
@@ -176,6 +178,10 @@ void StartGimbalTask(void const * argument)
 //				Motor_Dm_Control(Down_yaw,target_position);
 //        Motor_Dm_Mit_Control(Down_yaw,0,0,Down_yaw->output);
 //				Motor_Dm_Transmit(Down_yaw);
+			//Pitch轴
+					Motor_Dm_Control(pitch,target_position);
+          Motor_Dm_Mit_Control(pitch,0,0,pitch->output);
+				  Motor_Dm_Transmit(pitch);
 			
 			//大疆
 				Motor_Dji_Control(Up_yaw,target_up_position);
@@ -185,6 +191,8 @@ void StartGimbalTask(void const * argument)
 				Motor_Dm_Cmd(Down_yaw,DM_CMD_MOTOR_DISABLE);
 				Motor_Dm_Transmit(Down_yaw);
 				Motor_Dji_Control(Up_yaw,target_position);//暂时的逻辑
+			//其他同理
+			
 				Up_yaw->output=0;
 				Motor_Dji_Transmit(Up_yaw);
 				break;
---
+++ Task/app_shooter_task.h
@@ -0,0 +1,26 @@
+#ifndef APP_SHOOTER_TASK_H
+#define APP_SHOOTER_TASK_H
+
+#include "FreeRTOS.h"
+#include "cmsis_os.h"
+
+
+#include "bsp_log.h"
+#include "bsp_can.h"
+
+
+#include "dev_motor_dji.h"
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // APP_SHOOTER_TASK_H
